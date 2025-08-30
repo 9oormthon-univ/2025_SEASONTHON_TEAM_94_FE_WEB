03696052ca4bb4afdb597023826c6c7ad3ad4a89
Yonghun Park <yw9142@gmail.com>
2025-08-30 11:05:43 +0900

    refactor: 스크롤 숨김 처리

--- app/root.tsx
+++ app/root.tsx
@@ -147,7 +147,7 @@ export default function App() {
         <main className="content">
           <Outlet />
         </main>
-        <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 flex justify-around items-center py-1">
+  <nav className="fixed bottom-0 left-0 right-0 bg-white border-t border-gray-200 flex justify-around items-center py-1 bottom-nav">
           <Link
             to="/expenses"
             className={`flex flex-col items-center py-1 px-4 ${
